2f4bf16d963ef9ed7874f481b07cc20ae18b7091
MADHIUKSHA-S <madhiuksha551@gmail.com>
2025-04-30 22:48:36 +0530

    page not found rectified4

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/.redirects', // NOTE: use .redirects, not _redirects
+          dest: '.' // copy to dist/
+        }
+      ]
+    })
+  ],
   base: '/',
-  server: {port: 5173}
+  server: { port: 5173 }
 })
